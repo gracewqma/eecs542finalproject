1a770f57704cab97314b087001edb7cdcddb5721
ntseng450 <nathan.tseng.3.14159@gmail.com>
2022-04-10 16:12:41 -0400

    add cutout/translation to CUT

--- cut/models/aug_class.py
+++ cut/models/aug_class.py
@@ -107,14 +107,60 @@ class AugmentClass:
 
     # def sharpen(input, sharpen_amount=2, probability=0.3):
     #     return transforms.RandomAdjustSharpness(sharpen_amount, probability)(input)
+    def rand_translation(self, x, use_saved_randoms =False, ratio=0.125):
+        print("rand_translation")
+        shift_x, shift_y = int(x.size(2) * ratio + 0.5), int(x.size(3) * ratio + 0.5)
+        if use_saved_randoms:
+            translation_x = self.saved_randoms['rand_translation']['x']
+            translation_y = self.saved_randoms['rand_translation']['y']
+        else:
+            translation_x = torch.randint(-shift_x, shift_x + 1, size=[x.size(0), 1, 1], device=x.device)
+            translation_y = torch.randint(-shift_y, shift_y + 1, size=[x.size(0), 1, 1], device=x.device)
+            self.saved_randoms['rand_translation']['x'] = translation_x
+            self.saved_randoms['rand_translation']['y'] = translation_y
+
+        grid_batch, grid_x, grid_y = torch.meshgrid(
+            torch.arange(x.size(0), dtype=torch.long, device=x.device),
+            torch.arange(x.size(2), dtype=torch.long, device=x.device),
+            torch.arange(x.size(3), dtype=torch.long, device=x.device),
+        )
+        grid_x = torch.clamp(grid_x + translation_x + 1, 0, x.size(2) + 1)
+        grid_y = torch.clamp(grid_y + translation_y + 1, 0, x.size(3) + 1)
+        x_pad = F.pad(x, [1, 1, 1, 1, 0, 0, 0, 0])
+        x = x_pad.permute(0, 2, 3, 1).contiguous()[grid_batch, grid_x, grid_y].permute(0, 3, 1, 2).contiguous()
+        return x
+
+    # from diff aug paper
+    def rand_cutout(self, x, use_saved_randoms =False,  ratio=0.5):
+        print("rand cutout")
+        cutout_size = int(x.size(2) * ratio + 0.5), int(x.size(3) * ratio + 0.5)
+        if use_saved_randoms:
+            offset_x = self.saved_randoms['rand_cutout']['x']
+            offset_y = self.saved_randoms['rand_cutout']['y']
+        else:
+            offset_x = torch.randint(0, x.size(2) + (1 - cutout_size[0] % 2), size=[x.size(0), 1, 1], device=x.device)
+            offset_y = torch.randint(0, x.size(3) + (1 - cutout_size[1] % 2), size=[x.size(0), 1, 1], device=x.device)
+            self.saved_randoms['rand_cutout']['x'] = offset_x
+            self.saved_randoms['rand_cutout']['y'] = offset_y
+        grid_batch, grid_x, grid_y = torch.meshgrid(
+            torch.arange(x.size(0), dtype=torch.long, device=x.device),
+            torch.arange(cutout_size[0], dtype=torch.long, device=x.device),
+            torch.arange(cutout_size[1], dtype=torch.long, device=x.device),
+        )
+        grid_x = torch.clamp(grid_x + offset_x - cutout_size[0] // 2, min=0, max=x.size(2) - 1)
+        grid_y = torch.clamp(grid_y + offset_y - cutout_size[1] // 2, min=0, max=x.size(3) - 1)
+        mask = torch.ones(x.size(0), x.size(2), x.size(3), dtype=x.dtype, device=x.device)
+        mask[grid_batch, grid_x, grid_y] = 0
+        x = x * mask.unsqueeze(1)
+        return x
 
 
 
 
     AUGMENT_FNS = {
         'color': [brightness_orig, saturation, contrast],
-        # 'translation': [rand_translation],
-        # 'cutout': [rand_cutout],
+        'translation': [rand_translation],
+        'cutout': [rand_cutout],
         # 'gaussian': [blur, sharpen],
         'affine': [rotation, hflip],
     }
